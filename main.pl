192b29ff483df7de33f8986da076556da18c3ca3
Carlos Carballido <carlos.carballido@alumnos.uvigo.es>
2024-04-09 17:47:46 +0200

    Add initial player data to main.pl

---
+++ main.pl
@@ -0,0 +1,11 @@
+% Base de Hechos
+% jugador(Rol, Nombre, Ataque, Defensa, Velocidad)
+jugador(medio, 'Sergio Busquets', 90, 85, 88).
+jugador(defensa, 'Sergio Ramos', 85, 80, 90).
+jugador(delantero, 'David Villa', 88, 82, 85).
+jugador(delantero, 'Fernando Torres', 87, 83, 87).
+jugador(delantero, 'Pedro Rodriguez', 87, 84, 90).
+jugador(medio, 'Andres Iniesta', 92, 85, 90).
+jugador(medio, 'Xavi Hernandez', 91, 86, 89).
+jugador(portero, 'Iker Casillas', 85, 75, 88).
+jugador(defensa, 'Gerard Pique', 84, 78, 86).
