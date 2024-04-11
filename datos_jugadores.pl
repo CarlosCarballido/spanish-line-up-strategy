6e13746f54aefca3e70393cf57d215004e8d78bf
Carlos Carballido <carlos.carballido@alumnos.uvigo.es>
2024-04-11 21:34:20 +0200

    Add datos_jugadores.pl file with player data

---
+++ datos_jugadores.pl
@@ -0,0 +1,74 @@
+% Definición de jugadores
+% jugador(Nombre, GolesTemporada, Asistencias, Promedio_goles, Precision_pases, Regates_partido, Recuperacion_partido, Precision_tiro, Precision_pases, Asistencias, Regates, Intercepciones).
+es_jugador('Kepa Arrizabalaga', 0, 0, 0.92, ).
+es_jugador('Robert Sánchez', 0, 0, 0.91, ).
+es_jugador('David Raya', 0, 0, 0.90, ).
+es_jugador('José L. Gayá', 0, 1, 0.92, ).
+es_jugador('Alejandro Balde', 0, 1, 0.89, ).
+es_jugador('Aymeric Laporte', 3, 3, 0.91, ).
+es_jugador('Iñigo Martínez', 0, 0, 0.87, ).
+es_jugador('Nacho Fernández', 0, 0, 0.90, ).
+es_jugador('David García', 0, 0, 0.85, ).
+es_jugador('Pedro Porro', 1, 0, 0.80, ).
+es_jugador('Dani Carvajal', 2, 3, 0.92, ).
+es_jugador('Rodri', 5, 7, 0.95, ).
+es_jugador('Martín Zubimendi', 0, 6, 0.81, ).
+es_jugador('Gavi', 1, 2, 0.87, ).
+es_jugador('Fabián Ruiz', 0, 4, 0.90, ).
+es_jugador('Mikel Merino', 0, 2, 0.91, ).
+es_jugador('Dani Ceballos', 0, 1, 0.78, ).
+es_jugador('Pedri', 3, 8, 0.97, ).
+es_jugador('Álvaro Morata', 15, 4, 0.82, ).
+es_jugador('Dani Olmo', 7, 2, 0.88, ).
+es_jugador('Nico Williams', 12, 4, 0.90, ).
+es_jugador('Bryan Gil', 4, 2, 0.86, ).
+es_jugador('Mikel Oyarzabal', 11, 3, 0.94, ).
+es_jugador('Iago Aspas', 13, 5, 0.90, ).
+es_jugador('Joselu', 10, 3, 0.93, ).
+es_jugador('Gerard Moreno', 9, 1, 0.91, ).
+
+% Definición de posiciones
+es_portero('Kepa Arrizabalaga')
+es_portero('Robert Sánchez')
+es_portero('David Raya')
+
+es_defensa('José L. Gayá')
+es_defensa('Alejandro Balde')
+es_defensa('Aymeric Laporte')
+es_defensa('Iñigo Martínez')
+es_defensa('Nacho Fernández')
+es_defensa('David García')
+es_defensa('Pedro Porro')
+es_defensa('Dani Carvajal')
+
+es_mediocentro('Rodri')
+es_mediocentro('Martín Zubimendi')
+es_mediocentro('Gavi')
+es_mediocentro('Fabián Ruiz')
+es_mediocentro('Mikel Merino')
+es_mediocentro('Dani Ceballos')
+es_mediocentro('Pedri')
+
+es_delantero('Álvaro Morata')
+es_delantero('Dani Olmo')
+es_delantero('Nico Williams')
+es_delantero('Bryan Gil')
+es_delantero('Mikel Oyarzabal')
+es_delantero('Iago Aspas')
+es_delantero('Joselu')
+es_delantero('Gerard Moreno')
+
+% Definicion de pie dominante
+es_zurdo('José L. Gayá')
+es_zurdo('Aymeric Laporte')
+es_zurdo('Iñigo Martínez')
+es_zurdo('Nacho Fernández')
+es_zurdo('Pedro Porro')
+es_zurdo('Martín Zubimendi')
+es_zurdo('Gavi')
+es_zurdo('Fabián Ruiz')
+es_zurdo('Mikel Merino')
+es_zurdo('Dani Ceballos')
+es_zurdo('Álvaro Morata')
+es_zurdo('Mikel Oyarzabal')
+es_zurdo('Joselu')
