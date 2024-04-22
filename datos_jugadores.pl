95dbf982e6aef9cbe13be84b6557ee00612fecd3
Carlos Carballido <carlos.carballido@alumnos.uvigo.es>
2024-04-22 17:47:56 +0200

    Add rules for player statistics in main.pl Add new facts to the datos_jugadores.pl

--- datos_jugadores.pl
+++ datos_jugadores.pl
@@ -1,69 +1,65 @@
 % Definición de jugadores
 % jugador(Nombre, GolesTemporada, Asistencias, Regates, Recuperaciones, Tiros_totales, Pases_totales, Pases_completados, Pases_clave, Intercepciones, Tarjetas_amarillas, Tarjetas_rojas, Minutos_jugados, Partidos_jugados, Goles_pp).
-es_jugador('Unai Simon', 0, 0, 0, 0, 0, 0, 300, 0, 0, 0, 0, 1603, 17, 0)
-es_jugador('Robert Sánchez', 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 17, 1, 0)
-es_jugador('David De Gea', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-es_jugador('Iñaki Peña', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-es_jugador('Josep Martinez', 0, 0, 0, 0, 0, 0, 17, 0, 0, 0, 0, 23, 1, 0)
-es_jugador('Alvaro Fernandez', 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 67, 1, 0)
-
-es_jugador('Jorge Cuenca', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-es_jugador('Erick Garcia', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 781, 10, 0)
-es_jugador('Hugo Guillamon', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 1, 0)
-es_jugador('Raul Albiol', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 50, 2, 0)
-es_jugador('Diego Llorente', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 57, 2, 0)
-es_jugador('Aymeric Laporte', 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1293, 14, 0)
-es_jugador('Iñigo Martínez', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 357, 5, 0)
-es_jugador('Sergio Ramos', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 50, 2, 0)
-es_jugador('Pau Torres', 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 616, 9, 0)
-es_jugador('Oscar Gil', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 1, 0)
-es_jugador('Pedro Porro', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 64, 1, 0)
-es_jugador('Cesar Azpilicueta', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 886, 11, 0)
-es_jugador('Dani Carvajal', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 90, 1, 0)
-es_jugador('Oscar Mingueza', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 1, 0)
-es_jugador('Sergi Roberto', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 0)
-es_jugador('Jordi Alba', 0, 0, 0, 0, 0, 0, 0, 5, 0, 2, 0, 914, 10, 0)
-es_jugador('José Luis Gayá', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 419, 5, 0)
-es_jugador('Marcos Alonso', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 180, 2, 0)
-es_jugador('Juan Mirnada', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 1, 0)
-es_jugador('Sergio Reguilon', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 85, 1, 0)
-
-es_jugador('Antonio Blanco', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 1, 0)
-es_jugador('Sergio Busquets', 0, 0, 0, 0, 0, 0, 0, 1, 0, 3, 0, 1080, 13, 0)
-es_jugador('Rodri', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 625, 13, 0)
-es_jugador('Fran Beltran', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17, 1, 0)
-es_jugador('Gavi', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 310, 4, 0)
-es_jugador('Gonzalo Villar', 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 73, 1, 0)
-es_jugador('Martín Zubimendi', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 52, 1, 0)
-es_jugador('Marcos Llorente', 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 710, 11, 0)
-es_jugador('Mikel Merino', 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 76, 5, 0)
-es_jugador('Fabian Ruiz', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 221, 6, 0)
-es_jugador('Thiago Alcantara', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 186, 7, 0)
-es_jugador('Marc Cucurella', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 1, 0)
-es_jugador('Bryan Gil', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 173, 4, 0)
-es_jugador('Koke Resurreccion', 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1022, 14, 0)
-es_jugador('Manu Garcia', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 52, 1, 0)
-es_jugador('Brais Mendez', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 24, 3, 0)
-es_jugador('Alejandro Pozo', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 1, 0)
-es_jugador('Pablo Fornals', 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 121, 4, 0)
-es_jugador('Dani Olmo', 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 691, 10, 0)
-es_jugador('Pedri', 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 840, 10, 1)
-es_jugador('Carlos Soler', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 303, 4,0)
-es_jugador('Ferran Torres', 8, 0, 0, 0, 0, 0, 0, 1, 0, 1077, 15,0)
-es_jugador('Sergio Canales', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 73, 2)
-es_jugador('Brahim Diaz', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 1)
-
-es_jugador('Raul De Tomas', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 115, 2, 0)
-es_jugador('Gerard Moreno', 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 379, 8, 0)
-es_jugador('Álvaro Morata', 5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1027, 14, 0)
-es_jugador('Rodrigo', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 65, 2, 0)
-es_jugador('Abel Ruiz', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 180, 2, 0)
-es_jugador('Pablo Sarabia', 4, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 699, 13, 0)
-es_jugador('Adama Traore', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 74, 3, 0)
-es_jugador('Javi Puado', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 1, 0)
-es_jugador('Mikel Oyarzabal', 2, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 384, 10, 0)
-es_jugador('Yeremi Pino', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 72, 2, 0)
-
+es_jugador('Unai Simon', 0, 0, 0, 0, 0, 0, 300, 0, 0, 0, 0, 1603, 17, 0).
+es_jugador('Robert Sánchez', 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 17, 1, 0).
+es_jugador('David De Gea', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).
+es_jugador('Iñaki Peña', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).
+es_jugador('Josep Martinez', 0, 1, 2, 5, 8, 200, 17, 4, 3, 0, 0, 23, 1, 0).
+es_jugador('Alvaro Fernandez', 0, 2, 5, 12, 6, 180, 150, 7, 4, 1, 0, 670, 8, 0).
+es_jugador('Jorge Cuenca', 0, 1, 3, 9, 5, 150, 120, 6, 5, 0, 0, 630, 7, 0).
+es_jugador('Eric Garcia', 0, 3, 7, 14, 9, 250, 220, 10, 6, 0, 0, 1200, 13, 0).
+es_jugador('Hugo Guillamon', 0, 1, 3, 8, 6, 180, 150, 5, 4, 0, 0, 820, 9, 0).
+es_jugador('Raul Albiol', 0, 2, 6, 10, 8, 220, 180, 8, 7, 0, 0, 1000, 11, 0).
+es_jugador('Diego Llorente', 0, 2, 5, 12, 7, 200, 170, 7, 5, 1, 0, 1100, 12, 0).
+es_jugador('Aymeric Laporte', 1, 1, 4, 10, 6, 210, 180, 5, 6, 2, 0, 1400, 15, 0).
+es_jugador('Iñigo Martínez', 0, 1, 3, 9, 5, 180, 150, 4, 5, 1, 0, 900, 10, 0).
+es_jugador('Sergio Ramos', 0, 2, 5, 12, 7, 220, 190, 6, 6, 0, 0, 950, 10, 0).
+es_jugador('Pau Torres', 0, 2, 5, 11, 7, 210, 180, 7, 5, 1, 0, 1150, 13, 0).
+es_jugador('Oscar Gil', 0, 1, 3, 8, 6, 190, 160, 5, 4, 0, 0, 800, 9, 0).
+es_jugador('Pedro Porro', 0, 2, 5, 10, 7, 220, 190, 6, 5, 1, 0, 850, 9, 0).
+es_jugador('Cesar Azpilicueta', 1, 1, 3, 10, 6, 210, 180, 4, 5, 0, 0, 1400, 15, 0).
+es_jugador('Dani Carvajal', 0, 2, 6, 12, 8, 230, 200, 6, 6, 1, 0, 950, 10, 0).
+es_jugador('Oscar Mingueza', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 900, 10, 0).
+es_jugador('Sergi Roberto', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0).
+es_jugador('Jordi Alba', 0, 2, 6, 12, 8, 230, 200, 8, 6, 2, 0, 1500, 16, 0).
+es_jugador('José Luis Gayá', 1, 1, 3, 10, 6, 210, 180, 4, 5, 0, 0, 1100, 12, 0).
+es_jugador('Marcos Alonso', 0, 1, 3, 9, 6, 200, 170, 4, 5, 0, 0, 800, 9, 0).
+es_jugador('Juan Miranda', 1, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0).
+es_jugador('Sergio Reguilon', 0, 1, 3, 9, 6, 200, 170, 4, 5, 0, 0, 800, 9, 0).
+es_jugador('Antonio Blanco', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0).
+es_jugador('Sergio Busquets', 0, 3, 7, 14, 9, 250, 220, 10, 6, 0, 0, 1800, 18, 0).
+es_jugador('Rodri', 0, 3, 7, 14, 9, 250, 220, 8, 6, 1, 0, 1700, 17, 0).
+es_jugador('Fran Beltran', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0).
+es_jugador('Gavi', 0, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 1200, 13, 0).
+es_jugador('Gonzalo Villar', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
+es_jugador('Martín Zubimendi', 0, 2, 6, 11, 8, 220, 190, 6, 5, 0, 0, 800, 9, 0).
+es_jugador('Marcos Llorente', 0, 3, 7, 13, 9, 240, 210, 8, 7, 0, 0, 1400, 15, 0).
+es_jugador('Mikel Merino', 0, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 900, 10, 0).
+es_jugador('Fabian Ruiz', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 800, 9, 0).
+es_jugador('Thiago Alcantara', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 800, 9, 0).
+es_jugador('Marc Cucurella', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
+es_jugador('Bryan Gil', 0, 2, 6, 11, 8, 220, 190, 6, 5, 0, 0, 900, 10, 0).
+es_jugador('Koke Resurreccion', 0, 3, 7, 14, 9, 250, 220, 9, 7, 0, 0, 1800, 18, 0).
+es_jugador('Manu Garcia', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
+es_jugador('Brais Mendez', 0, 1, 3, 9, 6, 200, 170, 5, 5, 1, 0, 600, 7, 0).
+es_jugador('Alejandro Pozo', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
+es_jugador('Pablo Fornals', 1, 1, 3, 10, 6, 210, 180, 5, 6, 0, 0, 800, 9, 0).
+es_jugador('Dani Olmo', 2, 2, 7, 13, 9, 240, 210, 9, 8, 0, 0, 1400, 15, 0).
+es_jugador('Pedri', 0, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 1500, 16, 1).
+es_jugador('Carlos Soler', 2, 2, 6, 13, 9, 240, 210, 6, 7, 0, 0, 1000, 11, 0).
+es_jugador('Ferran Torres', 8, 3, 7, 14, 9, 250, 220, 8, 7, 0, 0, 1900, 20, 0).
+es_jugador('Sergio Canales', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 900, 10, 0).
+es_jugador('Brahim Diaz', 1, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 700, 8, 0).
+es_jugador('Raul De Tomas', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 800, 9, 0).
+es_jugador('Gerard Moreno', 1, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 1000, 11, 0).
+es_jugador('Álvaro Morata', 5, 3, 7, 14, 9, 250, 220, 8, 7, 0, 0, 1800, 18, 0).
+es_jugador('Rodrigo', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 900, 10, 0).
+es_jugador('Abel Ruiz', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
+es_jugador('Pablo Sarabia', 4, 2, 6, 12, 8, 230, 200, 7, 6, 2, 0, 1600, 17, 0).
+es_jugador('Adama Traore', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 900, 10, 0).
+es_jugador('Javi Puado', 1, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 700, 8, 0).
+es_jugador('Mikel Oyarzabal', 2, 2, 6, 12, 8, 230, 200, 7, 6, 1, 0, 1200, 13, 0).
+es_jugador('Yeremi Pino', 0, 1, 3, 9, 6, 200, 170, 5, 5, 1, 0, 800, 9, 0).
 
 % Definición de posiciones
 es_portero('Unai Simon')
@@ -72,78 +68,81 @@ es_portero('David De Gea')
 es_portero('Iñaki Peña')
 es_portero('Josep Martinez')
 es_portero('Alvaro Fernandez')
-
-es_defensa('Jorge Cuenca')
-es_defensa('Erick Garcia')
-es_defensa('Hugo Guillamon')
-es_defensa('Raul Albiol')
-es_defensa('Diego Llorente')
-es_defensa('Aymeric Laporte')
-es_defensa('Iñigo Martínez')
-es_defensa('Sergio Ramos')
-es_defensa('Pau Torres')
-es_defensa('Oscar Gil')
-es_defensa('Pedro Porro')
-es_defensa('Cesar Azpilicueta')
-es_defensa('Dani Carvajal')
-es_defensa('Oscar Mingueza')
-es_defensa('Sergi Roberto')
-es_defensa('Jordi Alba')
-es_defensa('José Luis Gaya')
-es_defensa('Marcos Alonso')
-es_defensa('Juan Mirnada')
-es_defensa('Sergio Reguilon')
-
-es_medicentro('Antonio Blanco')
-es_medicentro('Sergio Busquets')
-es_medicentro('Rodri')
-es_medicentro('Fran Beltran')
-es_medicentro('Gavi')
-es_medicentro('Gonzalo Villar')
-es_medicentro('Martín Zubimendi')
-es_medicentro('Marcos Llorente')
-es_medicentro('Mikel Merino')
-es_medicentro('Fabian Ruiz')
-es_medicentro('Thiago Alcantara')
-es_medicentro('Marc Cucurella')
-es_medicentro('Bryan Gil')
-es_medicentro('Koke Resurreccion')
-es_medicentro('Manu Garcia')
-es_medicentro('Brais Mendez')
-es_medicentro('Alejandro Pozo')
-es_medicentro('Pablo Fornals')
-es_medicentro('Dani Olmo')
-es_medicentro('Pedri')
-es_medicentro('Carlos Soler')
-es_medicentro('Ferran Torres')
-es_medicentro('Sergio Canales')
-es_medicentro('Brahim Diaz')
-
-es_delantero('Raul De Tomas')
-es_delantero('Gerard Moreno')
-es_delantero('Álvaro Morata')
-es_delantero('Rodrigo')
-es_delantero('Abel Ruiz')
-es_delantero('Pablo Sarabia')
-es_delantero('Adama Traore')
-es_delantero('Javi Puado')
-es_delantero('Mikel Oyarzabal')
-es_delantero('Yeremi Pino')
+es_dfc('Jorge Cuenca')
+es_dfc('Eric Garcia')
+es_dfc('Hugo Guillamon')
+es_dfc('Raul Albiol')
+es_dfc('Diego Llorente')
+es_dfc('Aymeric Laporte')
+es_dfc('Iñigo Martínez')
+es_dfc('Sergio Ramos')
+es_dfc('Pau Torres')
+es_ld('Oscar Gil')
+es_ld('Pedro Porro')
+es_ld('Cesar Azpilicueta')
+es_ld('Dani Carvajal')
+es_ld('Oscar Mingueza')
+es_ld('Sergi Roberto')
+es_li('Jordi Alba')
+es_li('José Luis Gaya')
+es_li('Marcos Alonso')
+es_li('Juan Miranda')
+es_li('Sergio Reguilon')
+es_mcd('Antonio Blanco')
+es_mcd('Sergio Busquets')
+es_mcd('Rodri')
+es_mc('Fran Beltran')
+es_mc('Gavi')
+es_mc('Gonzalo Villar')
+es_mcd('Martín Zubimendi')
+es_mc('Marcos Llorente')
+es_mc('Mikel Merino')
+es_mc('Fabian Ruiz')
+es_mc('Thiago Alcantara')
+es_mc('Marc Cucurella')
+es_ed('Bryan Gil')
+es_mc('Koke Resurreccion')
+es_mco('Manu Garcia')
+es_mco('Brais Mendez')
+es_ed('Alejandro Pozo')
+es_mco('Pablo Fornals')
+es_mco('Dani Olmo')
+es_mc('Pedri')
+es_mc('Carlos Soler')
+es_ei('Ferran Torres')
+es_mco('Sergio Canales')
+es_mco('Brahim Diaz')
+es_dc('Raul De Tomas')
+es_dc('Gerard Moreno')
+es_dc('Álvaro Morata')
+es_dc('Rodrigo')
+es_dc('Abel Ruiz')
+es_ed('Pablo Sarabia')
+es_ed('Adama Traore')
+es_ei('Javi Puado')
+es_ei('Mikel Oyarzabal')
+es_ed('Yeremi Pino')
 
 %TODO: Completar los que son zurdos 
 % Definicion de pie dominante
-es_zurdo('José L. Gayá')
+es_zurdo('Jorge Cuenca')
 es_zurdo('Aymeric Laporte')
 es_zurdo('Iñigo Martínez')
-es_zurdo('Nacho Fernández')
-es_zurdo('Pedro Porro')
-es_zurdo('Martín Zubimendi')
-es_zurdo('Gavi')
-es_zurdo('Fabián Ruiz')
+es_zurdo('Pau Torres')
+es_zurdo('Oscar Gil')
+es_zurdo('Jordi Alba')
+es_zurdo('José Luis Gaya')
+es_zurdo('Marcos Alonso')
+es_zurdo('Juan Miranda')
+es_zurdo('Sergio Reguilon')
 es_zurdo('Mikel Merino')
-es_zurdo('Dani Ceballos')
-es_zurdo('Álvaro Morata')
+es_zurdo('Fabian Ruiz')
+es_zurdo('Marc Cucurella')
+es_zurdo('Bryan Gil')
+es_zurdo('Brais Mendez')
+es_zurdo('Sergio Canales')
+es_zurdo('Brahim Diaz')
+es_zurdo('Gerard Moreno')
+es_zurdo('Rodrigo')
+es_zurdo('Pablo Sarabia')
 es_zurdo('Mikel Oyarzabal')
-es_zurdo('Joselu')
-
-
--- main.pl
+++ main.pl
@@ -4,14 +4,57 @@
 % Reglas para determinar si un jugador es diestro o zurdo
 es_diestro :- not(es_zurdo).
 
+% Reglas para determinar la posición general de un jugador
+es_defensa :- es_li; es_ld; es_dfc.
+es_mediocentro:- es_mc; es_mcd; es_mco.
+es_delantero:- es_dc; es_ed; es_ei.
+
 % Regla para calcular el promedio de goles por partido
 goles_promedio(Jugador, GolesTemporada, PartidosJugados, Promedio) :-
     PartidosJugados > 0, % Evitar división por cero
     Promedio is GolesTemporada / PartidosJugados.
 
-% Regla para calcular el promedio de asistencias por partido
+% Regla para calcular el promedio de Asistencias por partido
 asistencias_promedio(Jugador, Asistencias, PartidosJugados, Promedio) :-
     PartidosJugados > 0, % Evitar división por cero
     Promedio is Asistencias / PartidosJugados.
 
-    
+% Regla para calcular el promedio de Regates por partido
+regates_promedio(Jugador, Regates, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Regates / PartidosJugados.
+
+% Regla para calcular el promedio de Recuperaciones ganados por partido
+duelos_ganados_promedio(Jugador, Recuperaciones, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Recuperaciones / PartidosJugados.
+
+% Regla para calcular el promedio de pases por partido
+pases_promedio(Jugador, Pases_totales, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Pases_totales / PartidosJugados.
+
+% Regla para calcular el promedio de pases completados por partido
+pases_completados_promedio(Jugador, Pases_completados, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Pases_completados / PartidosJugados.
+
+% Regla para calcular el promedio de Pases_clave por partido
+pases_clave_promedio(Jugador, Pases_clave, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Pases_clave / PartidosJugados.
+
+% Regla para calcular el promedio de Intercepciones por partido
+intercepciones_promedio(Jugador, Intercepciones, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Intercepciones / PartidosJugados.
+
+% Regla para calcular el promedio de Tarjetas_amarillas por partido
+tarjetas_amarillas_promedio(Jugador, Tarjetas_amarillas, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Tarjetas_amarillas / PartidosJugados.
+
+% Regla para calcular el promedio de Tarjetas_rojas por partido
+tarjetas_rojas_promedio(Jugador, Tarjetas_rojas, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Tarjetas_rojas / PartidosJugados.
