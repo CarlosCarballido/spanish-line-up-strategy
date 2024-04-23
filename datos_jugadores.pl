446931bbb31f832f942b355dad554ee64708c174
marinaacastroo <marinaacastroo24@gmail.com>
2024-04-23 12:10:22 +0200

    Add new player statistics calculations in main.pl

--- datos_jugadores.pl
+++ datos_jugadores.pl
@@ -1,65 +1,80 @@
 % Definición de jugadores
-% jugador(Nombre, GolesTemporada, Asistencias, Regates, Recuperaciones, Tiros_totales, Pases_totales, Pases_completados, Pases_clave, Intercepciones, Tarjetas_amarillas, Tarjetas_rojas, Minutos_jugados, Partidos_jugados, Goles_pp).
-es_jugador('Unai Simon', 0, 0, 0, 0, 0, 0, 300, 0, 0, 0, 0, 1603, 17, 0).
-es_jugador('Robert Sánchez', 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 17, 1, 0).
-es_jugador('David De Gea', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).
-es_jugador('Iñaki Peña', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).
-es_jugador('Josep Martinez', 0, 1, 2, 5, 8, 200, 17, 4, 3, 0, 0, 23, 1, 0).
-es_jugador('Alvaro Fernandez', 0, 2, 5, 12, 6, 180, 150, 7, 4, 1, 0, 670, 8, 0).
-es_jugador('Jorge Cuenca', 0, 1, 3, 9, 5, 150, 120, 6, 5, 0, 0, 630, 7, 0).
-es_jugador('Eric Garcia', 0, 3, 7, 14, 9, 250, 220, 10, 6, 0, 0, 1200, 13, 0).
-es_jugador('Hugo Guillamon', 0, 1, 3, 8, 6, 180, 150, 5, 4, 0, 0, 820, 9, 0).
-es_jugador('Raul Albiol', 0, 2, 6, 10, 8, 220, 180, 8, 7, 0, 0, 1000, 11, 0).
-es_jugador('Diego Llorente', 0, 2, 5, 12, 7, 200, 170, 7, 5, 1, 0, 1100, 12, 0).
-es_jugador('Aymeric Laporte', 1, 1, 4, 10, 6, 210, 180, 5, 6, 2, 0, 1400, 15, 0).
-es_jugador('Iñigo Martínez', 0, 1, 3, 9, 5, 180, 150, 4, 5, 1, 0, 900, 10, 0).
-es_jugador('Sergio Ramos', 0, 2, 5, 12, 7, 220, 190, 6, 6, 0, 0, 950, 10, 0).
-es_jugador('Pau Torres', 0, 2, 5, 11, 7, 210, 180, 7, 5, 1, 0, 1150, 13, 0).
-es_jugador('Oscar Gil', 0, 1, 3, 8, 6, 190, 160, 5, 4, 0, 0, 800, 9, 0).
-es_jugador('Pedro Porro', 0, 2, 5, 10, 7, 220, 190, 6, 5, 1, 0, 850, 9, 0).
-es_jugador('Cesar Azpilicueta', 1, 1, 3, 10, 6, 210, 180, 4, 5, 0, 0, 1400, 15, 0).
-es_jugador('Dani Carvajal', 0, 2, 6, 12, 8, 230, 200, 6, 6, 1, 0, 950, 10, 0).
-es_jugador('Oscar Mingueza', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 900, 10, 0).
-es_jugador('Sergi Roberto', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0).
-es_jugador('Jordi Alba', 0, 2, 6, 12, 8, 230, 200, 8, 6, 2, 0, 1500, 16, 0).
-es_jugador('José Luis Gayá', 1, 1, 3, 10, 6, 210, 180, 4, 5, 0, 0, 1100, 12, 0).
-es_jugador('Marcos Alonso', 0, 1, 3, 9, 6, 200, 170, 4, 5, 0, 0, 800, 9, 0).
-es_jugador('Juan Miranda', 1, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0).
-es_jugador('Sergio Reguilon', 0, 1, 3, 9, 6, 200, 170, 4, 5, 0, 0, 800, 9, 0).
-es_jugador('Antonio Blanco', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0).
-es_jugador('Sergio Busquets', 0, 3, 7, 14, 9, 250, 220, 10, 6, 0, 0, 1800, 18, 0).
-es_jugador('Rodri', 0, 3, 7, 14, 9, 250, 220, 8, 6, 1, 0, 1700, 17, 0).
-es_jugador('Fran Beltran', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0).
-es_jugador('Gavi', 0, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 1200, 13, 0).
-es_jugador('Gonzalo Villar', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
-es_jugador('Martín Zubimendi', 0, 2, 6, 11, 8, 220, 190, 6, 5, 0, 0, 800, 9, 0).
-es_jugador('Marcos Llorente', 0, 3, 7, 13, 9, 240, 210, 8, 7, 0, 0, 1400, 15, 0).
-es_jugador('Mikel Merino', 0, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 900, 10, 0).
-es_jugador('Fabian Ruiz', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 800, 9, 0).
-es_jugador('Thiago Alcantara', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 800, 9, 0).
-es_jugador('Marc Cucurella', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
-es_jugador('Bryan Gil', 0, 2, 6, 11, 8, 220, 190, 6, 5, 0, 0, 900, 10, 0).
-es_jugador('Koke Resurreccion', 0, 3, 7, 14, 9, 250, 220, 9, 7, 0, 0, 1800, 18, 0).
-es_jugador('Manu Garcia', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
-es_jugador('Brais Mendez', 0, 1, 3, 9, 6, 200, 170, 5, 5, 1, 0, 600, 7, 0).
-es_jugador('Alejandro Pozo', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
-es_jugador('Pablo Fornals', 1, 1, 3, 10, 6, 210, 180, 5, 6, 0, 0, 800, 9, 0).
-es_jugador('Dani Olmo', 2, 2, 7, 13, 9, 240, 210, 9, 8, 0, 0, 1400, 15, 0).
-es_jugador('Pedri', 0, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 1500, 16, 1).
-es_jugador('Carlos Soler', 2, 2, 6, 13, 9, 240, 210, 6, 7, 0, 0, 1000, 11, 0).
-es_jugador('Ferran Torres', 8, 3, 7, 14, 9, 250, 220, 8, 7, 0, 0, 1900, 20, 0).
-es_jugador('Sergio Canales', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 900, 10, 0).
-es_jugador('Brahim Diaz', 1, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 700, 8, 0).
-es_jugador('Raul De Tomas', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 800, 9, 0).
-es_jugador('Gerard Moreno', 1, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 1000, 11, 0).
-es_jugador('Álvaro Morata', 5, 3, 7, 14, 9, 250, 220, 8, 7, 0, 0, 1800, 18, 0).
-es_jugador('Rodrigo', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 900, 10, 0).
-es_jugador('Abel Ruiz', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0).
-es_jugador('Pablo Sarabia', 4, 2, 6, 12, 8, 230, 200, 7, 6, 2, 0, 1600, 17, 0).
-es_jugador('Adama Traore', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 900, 10, 0).
-es_jugador('Javi Puado', 1, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 700, 8, 0).
-es_jugador('Mikel Oyarzabal', 2, 2, 6, 12, 8, 230, 200, 7, 6, 1, 0, 1200, 13, 0).
-es_jugador('Yeremi Pino', 0, 1, 3, 9, 6, 200, 170, 5, 5, 1, 0, 800, 9, 0).
+% es_portero(Nombre, GolesTemporada, Asistencias, Regates, Recuperaciones, Tiros_totales, Pases_totales, Pases_completados, Pases_clave, Intercepciones, Tarjetas_amarillas, Tarjetas_rojas, Minutos_jugados, Partidos_jugados, Goles_pp, Goles_encajados, Penaltis, Penaltis_parados).
+es_jugador('Unai Simon', 0, 0, 0, 0, 0, 0, 300, 0, 0, 0, 0, 1603, 17, 0, 7, 2, 1).
+es_jugador('Robert Sánchez', 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 17, 1, 0, 1, 1, 0).
+es_jugador('David De Gea', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).
+es_jugador('Iñaki Peña', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).
+es_jugador('Josep Martinez', 0, 1, 2, 5, 8, 200, 17, 4, 3, 0, 0, 23, 1, 0, 2, 0, 0).
+es_jugador('Alvaro Fernandez', 0, 2, 5, 12, 6, 180, 150, 7, 4, 1, 0, 670, 8, 0, 4, 1, 0).
+
+promedio(Jugador, Promedio):-
+    es_portero(Jugador,_,_,_,_,_,_,_,_,_,_,_,_, Partidos_jugados, _, Goles_encajados, _, _),
+    Partidos_jugados > 0, % Evitar división por cero
+    Promedio_Goles_Encajados is Goles_encajados / Partidos_jugados,
+
+
+
+% es_jugador(Nombre, GolesTemporada, Asistencias, Regates, Recuperaciones, Tiros_totales, Pases_totales, Pases_completados, Pases_clave, Intercepciones, Tarjetas_amarillas, Tarjetas_rojas, Minutos_jugados, Partidos_jugados, Goles_pp, Goles_encajados, Penaltis, Penaltis_parados).
+es_jugador('Unai Simon', 0, 0, 0, 0, 0, 0, 300, 0, 0, 0, 0, 1603, 17, 0, 7, 2, 1).
+es_jugador('Robert Sánchez', 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 17, 1, 0, 1, 1, 0).
+es_jugador('David De Gea', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).
+es_jugador('Iñaki Peña', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).
+es_jugador('Josep Martinez', 0, 1, 2, 5, 8, 200, 17, 4, 3, 0, 0, 23, 1, 0, 2, 0, 0).
+es_jugador('Alvaro Fernandez', 0, 2, 5, 12, 6, 180, 150, 7, 4, 1, 0, 670, 8, 0, 4, 1, 0).
+es_jugador('Jorge Cuenca', 0, 1, 3, 9, 5, 150, 120, 6, 5, 0, 0, 630, 7, 0, 0, 0 , 0).
+es_jugador('Eric Garcia', 0, 3, 7, 14, 9, 250, 220, 10, 6, 0, 0, 1200, 13, 0, 0, 0 , 0).
+es_jugador('Hugo Guillamon', 0, 1, 3, 8, 6, 180, 150, 5, 4, 0, 0, 820, 9, 0, 0, 0 , 0).
+es_jugador('Raul Albiol', 0, 2, 6, 10, 8, 220, 180, 8, 7, 0, 0, 1000, 11, 0, 0, 0 , 0).
+es_jugador('Diego Llorente', 0, 2, 5, 12, 7, 200, 170, 7, 5, 1, 0, 1100, 12, 0, 0, 0 , 0).
+es_jugador('Aymeric Laporte', 1, 1, 4, 10, 6, 210, 180, 5, 6, 2, 0, 1400, 15, 0, 0, 0 , 0).
+es_jugador('Iñigo Martínez', 0, 1, 3, 9, 5, 180, 150, 4, 5, 1, 0, 900, 10, 0, 0, 0 , 0).
+es_jugador('Sergio Ramos', 0, 2, 5, 12, 7, 220, 190, 6, 6, 0, 0, 950, 10, 0, 0, 0 , 0).
+es_jugador('Pau Torres', 0, 2, 5, 11, 7, 210, 180, 7, 5, 1, 0, 1150, 13, 0, 0, 0 , 0).
+es_jugador('Oscar Gil', 0, 1, 3, 8, 6, 190, 160, 5, 4, 0, 0, 800, 9, 0, 0, 0 , 0).
+es_jugador('Pedro Porro', 0, 2, 5, 10, 7, 220, 190, 6, 5, 1, 0, 850, 9, 0, 0, 0 , 0).
+es_jugador('Cesar Azpilicueta', 1, 1, 3, 10, 6, 210, 180, 4, 5, 0, 0, 1400, 15, 0, 0, 0 , 0).
+es_jugador('Dani Carvajal', 0, 2, 6, 12, 8, 230, 200, 6, 6, 1, 0, 950, 10, 0, 0, 0 , 0).
+es_jugador('Oscar Mingueza', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 900, 10, 0, 0, 0 , 0).
+es_jugador('Sergi Roberto', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0, 0, 0 , 0).
+es_jugador('Jordi Alba', 0, 2, 6, 12, 8, 230, 200, 8, 6, 2, 0, 1500, 16, 0, 0, 0 , 0).
+es_jugador('José Luis Gayá', 1, 1, 3, 10, 6, 210, 180, 4, 5, 0, 0, 1100, 12, 0, 0, 0 , 0).
+es_jugador('Marcos Alonso', 0, 1, 3, 9, 6, 200, 170, 4, 5, 0, 0, 800, 9, 0, 0, 0 , 0).
+es_jugador('Juan Miranda', 1, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0, 0, 0 , 0).
+es_jugador('Sergio Reguilon', 0, 1, 3, 9, 6, 200, 170, 4, 5, 0, 0, 800, 9, 0, 0, 0 , 0).
+es_jugador('Antonio Blanco', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0, 0, 0 , 0).
+es_jugador('Sergio Busquets', 0, 3, 7, 14, 9, 250, 220, 10, 6, 0, 0, 1800, 18, 0, 0, 0 , 0).
+es_jugador('Rodri', 0, 3, 7, 14, 9, 250, 220, 8, 6, 1, 0, 1700, 17, 0, 0, 0 , 0).
+es_jugador('Fran Beltran', 0, 1, 3, 8, 6, 190, 160, 4, 4, 0, 0, 600, 7, 0, 0, 0 , 0).
+es_jugador('Gavi', 0, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 1200, 13, 0, 0, 0 , 0).
+es_jugador('Gonzalo Villar', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0, 0, 0 , 0).
+es_jugador('Martín Zubimendi', 0, 2, 6, 11, 8, 220, 190, 6, 5, 0, 0, 800, 9, 0, 0, 0 , 0).
+es_jugador('Marcos Llorente', 0, 3, 7, 13, 9, 240, 210, 8, 7, 0, 0, 1400, 15, 0, 0, 0 , 0).
+es_jugador('Mikel Merino', 0, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 900, 10, 0, 0, 0 , 0).
+es_jugador('Fabian Ruiz', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 800, 9, 0, 0, 0 , 0).
+es_jugador('Thiago Alcantara', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 800, 9, 0, 0, 0 , 0).
+es_jugador('Marc Cucurella', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0, 0, 0 , 0).
+es_jugador('Bryan Gil', 0, 2, 6, 11, 8, 220, 190, 6, 5, 0, 0, 900, 10, 0, 0, 0 , 0).
+es_jugador('Koke Resurreccion', 0, 3, 7, 14, 9, 250, 220, 9, 7, 0, 0, 1800, 18, 0, 0, 0 , 0).
+es_jugador('Manu Garcia', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0, 0, 0 , 0).
+es_jugador('Brais Mendez', 0, 1, 3, 9, 6, 200, 170, 5, 5, 1, 0, 600, 7, 0, 0, 0 , 0).
+es_jugador('Alejandro Pozo', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0, 0, 0 , 0).
+es_jugador('Pablo Fornals', 1, 1, 3, 10, 6, 210, 180, 5, 6, 0, 0, 800, 9, 0, 0, 0 , 0).
+es_jugador('Dani Olmo', 2, 2, 7, 13, 9, 240, 210, 9, 8, 0, 0, 1400, 15, 0, 0, 0 , 0).
+es_jugador('Pedri', 0, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 1500, 16, 1, 0, 0 , 0).
+es_jugador('Carlos Soler', 2, 2, 6, 13, 9, 240, 210, 6, 7, 0, 0, 1000, 11, 0, 0, 0 , 0).
+es_jugador('Ferran Torres', 8, 3, 7, 14, 9, 250, 220, 8, 7, 0, 0, 1900, 20, 0, 0, 0 , 0).
+es_jugador('Sergio Canales', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 900, 10, 0, 0, 0 , 0).
+es_jugador('Brahim Diaz', 1, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 700, 8, 0, 0, 0 , 0).
+es_jugador('Raul De Tomas', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 800, 9, 0, 0, 0 , 0).
+es_jugador('Gerard Moreno', 1, 2, 6, 12, 8, 230, 200, 7, 6, 0, 0, 1000, 11, 0, 0, 0 , 0).
+es_jugador('Álvaro Morata', 5, 3, 7, 14, 9, 250, 220, 8, 7, 0, 0, 1800, 18, 0, 0, 0 , 0).
+es_jugador('Rodrigo', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 900, 10, 0, 0, 0 , 0).
+es_jugador('Abel Ruiz', 0, 1, 3, 9, 6, 200, 170, 5, 5, 0, 0, 700, 8, 0, 0, 0 , 0).
+es_jugador('Pablo Sarabia', 4, 2, 6, 12, 8, 230, 200, 7, 6, 2, 0, 1600, 17, 0, 0, 0 , 0).
+es_jugador('Adama Traore', 0, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 900, 10, 0, 0, 0 , 0).
+es_jugador('Javi Puado', 1, 2, 6, 12, 8, 230, 200, 6, 6, 0, 0, 700, 8, 0, 0, 0 , 0).
+es_jugador('Mikel Oyarzabal', 2, 2, 6, 12, 8, 230, 200, 7, 6, 1, 0, 1200, 13, 0, 0, 0 , 0).
+es_jugador('Yeremi Pino', 0, 1, 3, 9, 6, 200, 170, 5, 5, 1, 0, 800, 9, 0, 0, 0 , 0).
 
 % Definición de posiciones
 es_portero('Unai Simon')
@@ -145,4 +160,4 @@ es_zurdo('Brahim Diaz')
 es_zurdo('Gerard Moreno')
 es_zurdo('Rodrigo')
 es_zurdo('Pablo Sarabia')
-es_zurdo('Mikel Oyarzabal')
+es_zurdo('Mikel Oyarzabal').
--- main.pl
+++ main.pl
@@ -9,6 +9,11 @@ es_defensa :- es_li; es_ld; es_dfc.
 es_mediocentro:- es_mc; es_mcd; es_mco.
 es_delantero:- es_dc; es_ed; es_ei.
 
+% Regla para determinar el promedio de minutos por partido
+minutos_promedio(Jugador, MinutosJugados, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is MinutosJugados / PartidosJugados.
+
 % Regla para calcular el promedio de goles por partido
 goles_promedio(Jugador, GolesTemporada, PartidosJugados, Promedio) :-
     PartidosJugados > 0, % Evitar división por cero
@@ -58,3 +63,52 @@ tarjetas_amarillas_promedio(Jugador, Tarjetas_amarillas, PartidosJugados, Promed
 tarjetas_rojas_promedio(Jugador, Tarjetas_rojas, PartidosJugados, Promedio) :-
     PartidosJugados > 0, % Evitar división por cero
     Promedio is Tarjetas_rojas / PartidosJugados.
+
+% Regla para calcular el promedio de Goles_encajados por partido
+goles_encajados_promedio(Jugador, Goles_encajados, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Goles_encajados / PartidosJugados.
+
+% Regla para calcular el promedio de Penaltis_parados por partido
+penaltis_parados_promedio(Jugador, Penaltis_parados, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Penaltis_parados / PartidosJugados.
+
+% Regla para calcular el promedio de Goles_pp por partido
+goles_pp_promedio(Jugador, Goles_pp, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Goles_pp / PartidosJugados.
+
+% Regla para calcular el promedio de Penaltis por partido
+penaltis_promedio(Jugador, Penaltis, PartidosJugados, Promedio) :-
+    PartidosJugados > 0, % Evitar división por cero
+    Promedio is Penaltis / PartidosJugados.
+
+rp(Jugador, Goles_encajados, Penaltis_promedio, Penaltis_parados_promedio, Duelos_ganados_promedio, Partidos_jugados):-
+    Goles_encajados(Jugador, Goles_encajados, PartidosJugados, Promedio),
+    
+
+
+
+% Regla para calcular el ranking de porteros
+ranking_porteros(Jugador, Goles_encajados, penaltis_promedio, penaltis_parados_promedio, duelos_ganados_promedio, pases_promedio, pases_completados_promedio, pases_clave_promedio, intercepciones_promedio, tarjetas_amarillas_promedio, tarjetas_rojas_promedio, goles_pp_promedio, Ranking) :-
+    Partidos_jugados > 0, % Evitar división por cero
+    Ranking is 2 * (penaltis_promedio - penaltis_parados_promedio) - Goles_encajados + duelos_ganados_promedio + (pases_promedio - pases_completados_promedio) + pases_clave_promedio + intercepciones_promedio - tarjetas_amarillas_promedio - tarjetas_rojas_promedio - goles_pp_promedio.
+
+% Regla para calcular el ranking de jugadores defensas
+ranking_defensas(Jugador, goles_pp_promedio, minutos_promedio, asistencias_promedio, regates_promedio, duelos_ganados_promedio, pases_promedio, pases_completados_promedio, pases_clave_promedio, intercepciones_promedio, tarjetas_amarillas_promedio, tarjetas_rojas_promedio, Ranking) :-
+    minutos_promedio > 0, % Evitar división por cero
+    es_defensa,
+    Ranking is asistencias_promedio + regates_promedio + 2 * duelos_ganados_promedio + 1.7 * (pases_promedio - pases_completados_promedio) + 1.5 * pases_clave_promedio + 2 * intercepciones_promedio + (0.1 * minutos_promedio) - 3 * tarjetas_amarillas_promedio - 3 *tarjetas_rojas_promedio - 3 *goles_pp_promedio.
+
+% Regla para calcular el ranking de jugadores mediocentros
+ranking_mediocentros(Jugador, goles_pp_promedio, minutos_promedio, asistencias_promedio, regates_promedio, duelos_ganados_promedio, pases_promedio, pases_completados_promedio, pases_clave_promedio, intercepciones_promedio, tarjetas_amarillas_promedio, tarjetas_rojas_promedio, Ranking) :-
+    minutos_promedio > 0, % Evitar división por cero
+    es_mediocentro,
+    Ranking is 1.7 * asistencias_promedio + 1.5 * regates_promedio + 1.7 * duelos_ganados_promedio + 2 * (pases_promedio - pases_completados_promedio) + 2 * pases_clave_promedio + 2 * intercepciones_promedio + (0.1 * minutos_promedio) - 2 * tarjetas_amarillas_promedio - 2 * tarjetas_rojas_promedio - 1.5 * goles_pp_promedio.
+
+% Regla para calcular el ranking de jugadores delanteros
+ranking_delanteros(Jugador, goles_pp_promedio, minutos_promedio, asistencias_promedio, regates_promedio, duelos_ganados_promedio, pases_promedio, pases_completados_promedio, pases_clave_promedio, intercepciones_promedio, tarjetas_amarillas_promedio, tarjetas_rojas_promedio, Ranking) :-
+    minutos_promedio > 0, % Evitar división por cero
+    es_delantero,
+    Ranking is 1.5 * asistencias_promedio + 1.5 * regates_promedio + 1.7 * duelos_ganados_promedio + 1.2 * (pases_promedio - pases_completados_promedio) + 1.7 * pases_clave_promedio + 1.2 * intercepciones_promedio + (0.1 * minutos_promedio) - tarjetas_amarillas_promedio - tarjetas_rojas_promedio - goles_pp_promedio.
